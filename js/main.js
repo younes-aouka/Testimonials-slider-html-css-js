667c941ffdbbcf0c3e61c0f400de70b356345fbb
Younes-aouka <youneseaouka@gmail.com>
2025-01-10 16:34:11 +0100

    enhancement

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 // light/dark mode part
+const changeModeButton = document.querySelector(".mode");
 const modeIcon = document.querySelector(".mode i");
 const lightModeIcon = "fa-solid fa-moon";
 const darkModeIcon = "fa-regular fa-sun";
 const modeSwitchSound = new Audio("sounds/modeSwitch.mp3");
-
+modeSwitchSound.volume = 0.1;
 
 function searchForOldMode(){
     if(localStorage.getItem("mode")==="DarkMode"){
@@ -11,19 +12,11 @@ function searchForOldMode(){
         modeIcon.className = darkModeIcon;
     }
 }
-
 searchForOldMode();
 
-async function playSwitchSound() {
-    try{
-        await modeSwitchSound.play();
-    }catch(e){
-        console.log(e);
-    }
-}
 
-document.querySelector(".mode").addEventListener("click",async function() {
-    await playSwitchSound();
+changeModeButton.addEventListener("click",async function(){
+    changeModeButton.disabled = true ;
     if (document.body.classList.contains("lightMode")) {
         document.body.classList.replace("lightMode", "DarkMode");
         modeIcon.className = darkModeIcon;  
@@ -32,18 +25,18 @@ document.querySelector(".mode").addEventListener("click",async function() {
         modeIcon.className = lightModeIcon;
     }
     localStorage.setItem("mode",document.body.className);
+    await modeSwitchSound.play();
+    changeModeButton.disabled = false ;
 });
 
-document.addEventListener("beforeunload",(_) => localStorage.setItem("mode",document.body.className))
-
 // testimonials part
+
 const testimonials = [
     {
         img: "images/testimonial-01.jpg",
         testimonial: "From the moment I engaged with Younes Company, I felt like more than just a client—I felt like part of an innovative family. After collaborating with their talented team, I discovered not just solutions, but inspired ideas that transformed my vision into reality" ,
         Name: "Walter"
     },
-    
     {
         img: "images/testimonial-02.jpg",
         testimonial: "I have tried a few software of this kind and younes company is the best by far!",
@@ -66,11 +59,9 @@ function changeTestimonial() {
         img.src = testimonials[index].img;
         text.textContent = testimonials[index].testimonial;
         Name.textContent = testimonials[index].Name;
-        if (index === 2) index = 0;
-        else index++;
+        index= (index+1)%3 ;
     },5000);
 }
-
 changeTestimonial();
 
 
